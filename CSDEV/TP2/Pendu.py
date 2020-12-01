b92551e79530af4e2493b1b77afc1f5462aa5085
John John <fare.zagh@gmail.com>
2020-12-01 12:29:14 +0100

    tp2

---
+++ CSDEV/TP2/Pendu.py
@@ -0,0 +1,65 @@
+import random
+
+def test():
+    print("c est une test")
+
+def lectureDoc():
+    fichier=open("bankdeMot.txt","r", encoding="utf8")
+    listMOt=fichier.readlines()
+    fichier.close()
+    for i in range(len(listMOt)):
+        listMOt[i]=listMOt[i].rstrip("\n")
+
+    return listMOt
+
+def Mot(listeMot):
+    i=random.randint(0, len(listeMot))
+    choix=listeMot[i]
+    lettre=[]
+    for lt in choix:
+        lettre.append(lt)
+    return lettre
+
+def Affichage(mot, lettreDevine):
+    
+    for lettre in mot:
+        if lettre in lettreDevine:
+            print(lettre, end=" ")
+        else:
+            print("_", end=" ")
+    print('')
+
+    
+def Utilisateur():
+    lettreUtilisateur=input("choisir une lettre: ")
+    
+    return lettreUtilisateur.lower()
+
+
+def Correspondance(motDevine,testUtilisateur,Dites , compteur):
+    
+    if testUtilisateur in motDevine:
+        occurence= motDevine.count(testUtilisateur)
+        for i in range(occurence):
+            Dites.append(testUtilisateur)
+        
+        Affichage(motDevine,Dites)
+        
+    else:
+        compteur=compteur-1
+        Affichage(motDevine,Dites)
+        print("il vous reste: ",compteur,' vies')
+    return Dites, compteur
+
+def FinDEPArtie(vie,motadeviner,lettreDejaDevine):
+    valRen=True
+    if vie==0:
+        valRen= False
+        print('loser')
+    elif len(motadeviner)== len(lettreDejaDevine):
+        valRen=False
+        print("bien joué")
+    return valRen   
+
+
+
---
+++ CSDEV/TP2/jeuxPendu.py
@@ -0,0 +1,42 @@
+import Pendu
+
+partie=True
+meilleurScore=0
+
+while partie:
+    vie=8
+    dejaDevine=[]
+    continuer=True
+    document=Pendu.lectureDoc()
+    toutesLettres=[]
+    motADeviner=Pendu.Mot(document)
+
+    dejaDevine, vie=Pendu.Correspondance(motADeviner,motADeviner[0], dejaDevine, vie)
+
+    print(motADeviner)
+
+    while continuer:
+        testU=Pendu.Utilisateur()
+        if testU not in toutesLettres:
+            toutesLettres.append(testU)
+            dejaDevine, vie =Pendu.Correspondance(motADeviner,testU, dejaDevine, vie)
+            continuer=Pendu.FinDEPArtie(vie,motADeviner,dejaDevine)
+            print(toutesLettres)
+        else:
+            print("la lettre a deja ete dite")  
+            print(toutesLettres) 
+    
+    if vie> meilleurScore:
+        meilleurScore=vie
+    
+    print("c'est fini")
+
+    nouvellePartie=input('voulez vous recommencer:(o/n): ')
+    if nouvellePartie.lower()=="n":
+        partie=False  
+
+print("merci bye")
+
+
+
+
